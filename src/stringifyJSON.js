3514eb8873d3db1b357c5dc98d0b0daa07552768
Tyler Ochiai <tyler.a.ochiai@gmail.com>
2014-05-25 22:52:07 -0700

    stringifyJSON passes tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,37 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+    if (obj === null) {
+        return "null";
+    } else if(typeof(obj) === "object") {
+        if(Array.isArray(obj)) {
+            var arrString = "[";
+            for (var i = 0; i < obj.length; i++) {
+                arrString += stringifyJSON(obj[i]) + ",";
+            }
+            arrString += "]";
+            arrString = arrString.replace(",]", "]");
+            return arrString;
+        } else {
+            var objString = "{";
+            for (var key in obj) {
+                // skip function and undefined values
+                if(typeof(obj[key]) !== "function" && obj[key] !== undefined) {
+                    objString += '\"' + key + '\":' + stringifyJSON(obj[key]) + ",";
+                }
+            }
+            objString += "}";
+            objString = objString.replace(',}', '}');
+            return objString;
+        }
+    } else if(typeof(obj) === "string") {
+        return '\"' + obj + '\"';
+    } else if(obj === undefined) {
+        return "";
+    } else if (typeof(obj) === 'function') {
+        return "";
+    } else {
+        // obj has to be number or boolean
+        return obj.toString();
+    }
 };
